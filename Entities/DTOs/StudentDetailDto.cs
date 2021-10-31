54a06b30b0c95f4ab3e087976f01940e156c129d
baran <baran.ibrahim@std.izu.edu.tr>
2021-10-31 03:02:57 +0300

    DTO(Data Transformation Object) yapısı eklendi. Gerekli dosyalar olmaması gereken yerlerden silindi.

---
+++ Entities/DTOs/StudentDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class StudentDetailDto:IDto
+    {
+        public Guid StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string TeacherName { get; set; }
+
+        public int Gender { get; set; }
+    }
+}
