adb7adf7c8bc90a789180317d806d069a5069fff
baran <baran.ibrahim@std.izu.edu.tr>
2021-10-31 03:01:39 +0300

    Teacher eklendi

---
+++ BusinessLogicLayer/Concrete/TeacherManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer.Abstract;
+using DataAccessLayer.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class TeacherManager : ITeacherService
+    {
+        ITeacherDal _teacherDal;
+
+        public TeacherManager(ITeacherDal teacherDal)
+        {
+            _teacherDal = teacherDal;
+        }
+
+        public bool AddTeacher(Teacher teacher)
+        {
+            // İş kodları
+            _teacherDal.Add(teacher);
+            return true;
+        }
+    }
+}
---
+++ DataAccessLayer/Abstract/ITeacherDal.cs
@@ -0,0 +1,12 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ITeacherDal: IEntityRepository<Teacher>
+    {
+    }
+}
