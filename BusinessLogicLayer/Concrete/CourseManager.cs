8cbd8ec6bb983a8f0733c49455e2be542c3e3aca
baran <baran.ibrahim@std.izu.edu.tr>
2021-11-03 19:41:31 +0300

    Course tablosu database'e ve programa eklendi.

---
+++ BusinessLogicLayer/Concrete/CourseManager.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLogicLayer.Abstract;
+using Core.Utilities.Results;
+using DataAccessLayer.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogicLayer.Concrete
+{
+    public class CourseManager : ICourseService
+    {
+        ICourseDal _courseDal;
+        public CourseManager(ICourseDal courseDal)
+        {
+            _courseDal = courseDal;
+        }
+
+        public IResult AddCourse(Course course)
+        {
+            _courseDal.Add(course);
+            return new SuccessResult();
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/EntityFramework/EfCourseDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccessLayer.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccessLayer.Concrete.EntityFramework
+{
+    public class EfCourseDal : EfEntityRepositoryBase<Course, IsorendaContext>, ICourseDal
+    {
+    }
+}
