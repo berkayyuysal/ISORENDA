33453ddb0f0016f1626cde406c8530de414511ca
baran <baran.ibrahim@std.izu.edu.tr>
2021-11-03 19:41:00 +0300

    WebAPI eklendi

---
+++ WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessLogicLayer.Abstract;
+using BusinessLogicLayer.Constants.Messages;
+using BusinessLogicLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        IStudentService _studentService;
+        public StudentsController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            var result = _studentService.GetAllStudents();
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(string id)
+        {
+            var _id = new Guid(id);
+            var result = _studentService.GetStudentById(_id);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("AddStudent")]
+        public IActionResult AddStudent(Student student)
+        {
+            var result = _studentService.AddStudent(student);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
