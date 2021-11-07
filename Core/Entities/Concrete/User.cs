3537f166e4958026bd680e011b7608ba50965029
baran <baran.ibrahim@std.izu.edu.tr>
2021-11-07 19:11:46 +0300

    Yetkilendirme, güvenlik için gerekli classlar eklendi. API tarafındaki appsettings.json dosyası bu güvenlik işlemleri için gerekli değişikliklere uğratıldı.

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public byte[] UserPasswordHash { get; set; }
+        public byte[] UserPasswordSalt { get; set; }
+        public bool UserStatus { get; set; }
+
+    }
+}
