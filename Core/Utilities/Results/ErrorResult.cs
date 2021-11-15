c3ca8fddeb8b6a3dcfe5917c93c02adcbaf8becd
baran <baran.ibrahim@std.izu.edu.tr>
2021-11-15 11:43:13 +0300

    ErrorResult'ta mesaj göndermeme hatası giderildi.

--- Core/Utilities/Results/ErrorResult.cs
+++ Core/Utilities/Results/ErrorResult.cs
@@ -6,7 +6,7 @@ namespace Core.Utilities.Results
 {
     public class ErrorResult: Result
     {
-        public ErrorResult(string message): base(false)
+        public ErrorResult(string message): base(false, message)
         {
 
         }
