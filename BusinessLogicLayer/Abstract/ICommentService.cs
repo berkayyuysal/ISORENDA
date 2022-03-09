d850376fc847dbd85777132f189f0049866f705b
Ibrahim Furkan Baran <baran.ibrahim.izu@gmail.com>
2022-03-09 20:11:59 +0300

    Comment ve Discount sisteme eklendi.

---
+++ BusinessLogicLayer/Abstract/ICommentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+
+namespace BusinessLogicLayer.Abstract
+{
+    public interface ICommentService
+    {
+        IResult Add(Comment comment);
+        IResult Update(Comment comment);
+        IResult Delete(Comment comment);
+        IDataResult<List<Comment>> GetComments();
+        IDataResult<Comment> GetCommentById(Guid commentId);
+        IDataResult<List<Comment>> GetCommentsByUserId(Guid userId);
+    }
+}
---
+++ WebAPI/Controllers/CommentController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogicLayer.Abstract;
+using Core.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CommentController : Controller
+    {
+        ICommentService _commentService;
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost("AddComment")]
+        public IActionResult AddComment(Comment comment)
+        {
+            var result = _commentService.Add(comment);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("UpdateComment")]
+        public IActionResult UpdateComment(Comment comment)
+        {
+            var result = _commentService.Update(comment);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("DeleteComment")]
+        public IActionResult DeleteComment(Comment comment)
+        {
+            var result = _commentService.Delete(comment);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("GetComments")]
+        public IActionResult GetComments()
+        {
+            var result = _commentService.GetComments();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("GetCommentById")]
+        public IActionResult GetCommentById(Guid commentId)
+        {
+            var result = _commentService.GetCommentById(commentId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("GetCommentsByUserId")]
+        public IActionResult GetCommentsByUserId(Guid userId)
+        {
+            var result = _commentService.GetCommentsByUserId(userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+    }
+}
