3cfffce0fc982eefaf5c0e85807ce1c75529f384
Ibrahim Furkan Baran <baran.ibrahim.izu@gmail.com>
2022-03-19 20:41:47 +0300

    LoginLog tamamlandı geliştirmeler yapılacak.

--- BusinessLogicLayer/Concrete/LoginLogManager.cs
+++ BusinessLogicLayer/Concrete/LoginLogManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using BusinessLogicLayer.Abstract;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
 using Core.Entities.Concrete;
 using Core.Utilities.Results;
 using DataAccessLayer.Abstract;
@@ -15,15 +17,21 @@ namespace BusinessLogicLayer.Concrete
             _loginLogDal = loginLogDal;
         }
 
+        [TransactionScopeAspect]
+        [PerformanceAspect(20)]
         public IResult Add(LoginLog loginLog, Guid userId)
         {
             loginLog.UserId = userId;
+            loginLog.LoginDate = DateTime.Now;
             _loginLogDal.Add(loginLog);
             return new SuccessResult();
         }
 
-        public IResult Update(LoginLog loginLog)
+        [TransactionScopeAspect]
+        [PerformanceAspect(20)]
+        public IResult UpdateForLogOut(LoginLog loginLog)
         {
+            loginLog.LogoutDate = DateTime.Now;
             _loginLogDal.Update(loginLog);
             return new SuccessResult();
         }
@@ -36,6 +44,16 @@ namespace BusinessLogicLayer.Concrete
                 return new SuccessDataResult<List<LoginLog>>(result);
             }
             return new ErrorDataResult<List<LoginLog>>();
+        }
+
+        public IDataResult<List<LoginLog>> GetLoginLogsByUserId(Guid userId)
+        {
+            var result = _loginLogDal.GetAll(l => l.UserId == userId);
+            if (result != null)
+            {
+                return new SuccessDataResult<List<LoginLog>>(result);
+            }
+            return new ErrorDataResult<List<LoginLog>>(result);
         }
 
         public IDataResult<LoginLog> GetLoginLogById(Guid loginLogId)
